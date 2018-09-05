53bb63a15e8bca152af32fed5fb5d0e814a93ddf
dukkhadevops <43010308+dukkhadevops@users.noreply.github.com>
2018-09-05 14:46:59 -0400

    Create DeleteFilesOlder365Days.ps1

---
+++ Scripts/DeleteFilesOlderXDays/DeleteFilesOlder365Days.ps1
@@ -0,0 +1,12 @@
+$limit = (Get-Date).AddDays(-365)
+$path = "D:\path"
+
+get-date
+
+# Delete files older than the $limit.
+Get-ChildItem -Path $path -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
+
+get-date
+
+# Delete any empty directories left behind after deleting the old files.
+# Get-ChildItem -Path $path -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
