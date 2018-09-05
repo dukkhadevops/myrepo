940b4486b70edc6c9c3cce41356035cf3ad78c5e
dukkhadevops <43010308+dukkhadevops@users.noreply.github.com>
2018-09-05 15:02:28 -0400

    Add files via upload

---
+++ Scripts/RemoveXPoweredBy/RemoveXPoweredBy.ps1
@@ -0,0 +1,14 @@
+﻿$computers = Get-Content -Path "C:\SVN\WindowsAdmins\Scripts\DV\RemoveXPoweredBy\computers.txt"
+
+Foreach ($target in $computers) {
+
+    Invoke-Command -computername $target -ScriptBlock{
+        Import-module WebAdministration
+        #Remove-WebConfigurationProperty -PSPath MACHINE/WEBROOT/APPHOST -Name . -Filter system.webServer/httpProtocol/customHeaders -AtElement @{name="x-powered-by" ; value='ASP.NET'} -ErrorAction Stop
+        #Remove-WebConfigurationProperty  -pspath 'MACHINE/WEBROOT/APPHOST'  -filter "system.webServer/httpProtocol/customHeaders" -name "." -AtElement @{name='x-powered-by'}
+        Remove-WebConfigurationProperty  -pspath 'MACHINE/WEBROOT/APPHOST/Dataverify.com'  -filter "system.webServer/httpProtocol/customHeaders" -name "." -AtElement @{name='x-powered-by'}
+        
+    }
+
+#endforeach
+}
