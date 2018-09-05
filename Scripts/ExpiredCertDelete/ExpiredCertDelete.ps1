af97e97baf90ab70241d1d98f3839411c7b11fa1
dukkhadevops <43010308+dukkhadevops@users.noreply.github.com>
2018-09-05 14:49:46 -0400

    Add files via upload

---
+++ Scripts/ExpiredCertDelete/ExpiredCertDelete.ps1
@@ -0,0 +1,39 @@
+﻿#Create Array of objs
+$objs = @()
+
+#Create new objects in the array for each type of certificate store
+$objs += New-Object System.Security.Cryptography.X509Certificates.X509Store("TrustedPublisher","LocalMachine")
+$objs += New-Object System.Security.Cryptography.X509Certificates.X509Store("ClientAuthIssuer","LocalMachine")
+$objs += New-Object System.Security.Cryptography.x509Certificates.x509Store("Remote Desktop","LocalMachine")
+$objs += New-Object System.Security.Cryptography.X509Certificates.X509Store("Root","LocalMachine")
+$objs += New-Object System.Security.Cryptography.X509Certificates.X509Store("TrustedDevices","LocalMachine")
+$objs += New-Object System.Security.Cryptography.X509Certificates.X509Store("CA","LocalMachine")
+$objs += New-Object System.Security.Cryptography.X509Certificates.X509Store("Windows Live ID Token Issuer","LocalMachine")
+$objs += New-Object System.Security.Cryptography.X509Certificates.X509Store("REQUEST","LocalMachine")
+$objs += New-Object System.Security.Cryptography.X509Certificates.X509Store("AuthRoot","LocalMachine")
+$objs += New-Object System.Security.Cryptography.X509Certificates.X509Store("AAD Token Issuer","LocalMachine")
+$objs += New-Object System.Security.Cryptography.X509Certificates.X509Store("FlightRoot","LocalMachine")
+$objs += New-Object System.Security.Cryptography.X509Certificates.X509Store("SmartCardRoot","LocalMachine")
+$objs += New-Object System.Security.Cryptography.X509Certificates.X509Store("TrustedPeople","LocalMachine")
+$objs += New-Object System.Security.Cryptography.X509Certificates.X509Store("AddressBook","LocalMachine")
+$objs += New-Object System.Security.Cryptography.x509Certificates.x509Store("My","LocalMachine")
+$objs += New-Object System.Security.Cryptography.X509Certificates.X509Store("CertificateAuthority","LocalMachine")
+$objs += New-Object System.Security.Cryptography.x509Certificates.x509Store("Trust","LocalMachine")
+$objs += New-Object System.Security.Cryptography.x509Certificates.x509Store("Homegroup Machine Certificates","LocalMachine")
+$objs += New-Object System.Security.Cryptography.x509Certificates.x509Store("SMS","LocalMachine")
+
+#For each store object in the array, open them all for read/write, look at the certificates and where expiration is less than today remove the certificate
+ForEach ($obj in $objs)
+{
+    $obj.Open("ReadWrite")
+
+    ForEach ($cert in $obj.certificates)
+    {
+      if ($cert.NotAfter -lt $(Get-Date))
+      {
+       $obj.Remove($cert)
+      }
+    }
+
+    $obj.Close()
+}
---
+++ Scripts/ExpiredCertDelete/ExpiredCertDeleteHelper.ps1
@@ -0,0 +1,51 @@
+﻿enable-psremoting -force
+#cd C:\SVN\WindowsAdmins\Scripts\ExpiredCertDelete
+
+$computers = Get-Content -Path "C:\SVN\WindowsAdmins\Scripts\ExpiredCertDelete\computers.txt"
+$scriptpath = "C:\SVN\WindowsAdmins\Scripts\ExpiredCertDelete\MattsExpiredCertDelete.ps1"
+
+#region LogWrite function
+        $logfile = "C:\SVN\WindowsAdmins\Scripts\ExpiredCertDelete\log.txt"
+        Function LogWrite{
+            Param ([string]$logstring)
+            $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+            $Line = "$Stamp -- $target -- $logstring"
+            Add-Content $logfile -Value $Line
+        }
+        #endregion
+
+Foreach ($target in $computers) {
+
+    If (Test-WSMan -ComputerName $target -ErrorAction Stop -Verbose){
+
+        $message = "connection to $target success"
+        echo " "
+        echo $message
+        LogWrite $message
+
+        Try {
+            $message = "trying cert delete"
+            echo $message
+            LogWrite $message
+            invoke-command -ComputerName $target -FilePath $scriptpath -ErrorAction Stop 
+            $message = "success!"
+            echo $message
+            LogWrite $message
+        }
+
+        Catch {
+            $message = "failed to delete expired certs"
+            echo $message
+            LogWrite $message
+            break
+        }
+    }
+    Else {
+        $message = "connection to $target failed. Try running winrm /quickconfig on the destination host and try again"
+        echo $message
+        LogWrite $message
+    }
+    $message = "done with $target"
+    echo $message
+    LogWrite $message
+}
