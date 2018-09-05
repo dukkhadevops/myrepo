29aeb01d6c24ef0c5083f1846ba5110832712c2d
dukkhadevops <43010308+dukkhadevops@users.noreply.github.com>
2018-09-05 14:55:40 -0400

    Add files via upload

---
+++ Scripts/IPRangeLookup/IPRangeLookup.ps1
@@ -0,0 +1,5 @@
+﻿$outpath = "C:\SVN\WindowsAdmins\Scripts\IPRangeLookup\output.txt"
+$subnet = "172.16.200"
+
+$stuff = 1..254 | ForEach-Object {Get-WmiObject Win32_PingStatus -Filter "Address='$subnet.$_' and Timeout=200 and ResolveAddressNames='true' and StatusCode=0" | select ProtocolAddress*}
+$stuff | Format-Table | Out-File $outpath
