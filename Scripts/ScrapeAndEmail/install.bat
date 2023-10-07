9685463c8beed29134109c788ea5746ada34d06b
dukkhadevops <dukkhadevops@gmail.com>
2023-10-07 09:35:09 -0400

    allow install.bat to echo

--- Scripts/ScrapeAndEmail/install.bat
+++ Scripts/ScrapeAndEmail/install.bat
@@ -1,4 +1,3 @@
-@echo off
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0InstallDependencies.ps1\"' -Verb RunAs"
 
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0CreateScheduledTasks.ps1\"' -Verb RunAs"
